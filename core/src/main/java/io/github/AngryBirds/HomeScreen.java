6573f127f869814490984f320dc1b38f61e6dc81
Sanchay Singh <sanchay072@gmail.com>
2024-10-19 11:50:11 +0530

    not working....

---
+++ core/src/main/java/io/github/AngryBirds/HomeScreen.java
@@ -0,0 +1,140 @@
+package io.github.AngryBirds;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class HomeScreen implements Screen {
+    private final io.github.AngryBirds.Main game;
+    private OrthographicCamera camera;
+    private Texture background;
+    private BitmapFont titleFont, buttonFont;
+    private Stage stage;
+    private ImageButton playButton, quitButton, settingsButton;
+    private SpriteBatch batch;
+
+    public HomeScreen(io.github.AngryBirds.Main game) {
+        this.game = game;
+        batch = new SpriteBatch();
+
+        // Setup the camera
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);  // Set a fixed resolution
+
+        // Load the background texture
+        background = new Texture(Gdx.files.internal("ui/angry_birds_bg.jpg"));
+
+        // Generate fonts from TTF files
+        FreeTypeFontGenerator generator1 = new FreeTypeFontGenerator(Gdx.files.internal("Comicartoon-3DExtrude.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter titleParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        titleParameter.size = 72;  // Title font size
+        titleParameter.color = Color.RED;
+        titleFont = generator1.generateFont(titleParameter);  // Generate title font
+        generator1.dispose();
+
+        FreeTypeFontGenerator generator2 = new FreeTypeFontGenerator(Gdx.files.internal("Comicartoon-Regular.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter buttonParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        buttonParameter.size = 36;  // Button font size
+        buttonParameter.color = Color.WHITE;
+        buttonFont = generator2.generateFont(buttonParameter);  // Generate button font
+        generator2.dispose();
+
+        // Initialize the stage
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        // Create buttons with textures
+        playButton = createButton("ui/play.png", 400, 240);
+        quitButton = createButton("ui/play.png", 400, 160);
+        settingsButton = createButton("ui/settings.jpg", 400, 80);
+
+        // Add buttons to the stage
+        stage.addActor(playButton);
+        stage.addActor(quitButton);
+        stage.addActor(settingsButton);
+    }
+
+    private ImageButton createButton(String texturePath, float x, float y) {
+        Texture buttonTexture = new Texture(Gdx.files.internal(texturePath));
+        Drawable drawable = new TextureRegionDrawable(buttonTexture);
+        ImageButton button = new ImageButton(drawable);
+        button.setPosition(x - button.getWidth() / 2, y - button.getHeight() / 2);  // Center the button
+        return button;
+    }
+
+    @Override
+    public void show() {
+        // Not needed for now
+    }
+
+    @Override
+    public void render(float delta) {
+        // Clear the screen with a black background
+        ScreenUtils.clear(0, 0, 0, 1);
+
+        // Update the camera
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        // Draw the background and title
+        batch.begin();
+        batch.draw(background, 0, 0, 800, 480);  // Draw background full screen
+        titleFont.draw(batch, "Angry Birds", 400 - 150, 400);  // Draw title at the center
+        batch.end();
+
+        // Draw the buttons (handled by the stage)
+        stage.act(delta);
+        stage.draw();
+
+        // Handle button clicks
+//        if (playButton.isPressed()) {
+//            game.setScreen(new GameScreen(game));  // Switch to GameScreen
+//        } else if (quitButton.isPressed()) {
+//            Gdx.app.exit();  // Quit the game
+//        } else if (settingsButton.isPressed()) {
+//            game.setScreen(new SettingsScreen(game));  // Switch to SettingsScreen
+//        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+        // Not needed for now
+    }
+
+    @Override
+    public void resume() {
+        // Not needed for now
+    }
+
+    @Override
+    public void hide() {
+        // Not needed for now
+    }
+
+    @Override
+    public void dispose() {
+        // Dispose of assets
+        batch.dispose();
+        background.dispose();
+        titleFont.dispose();
+        buttonFont.dispose();
+        stage.dispose();
+    }
+}
--- core/src/main/java/io/github/AngryBirds/Main.java
+++ core/src/main/java/io/github/AngryBirds/Main.java
@@ -1,182 +1,47 @@
+
 package io.github.AngryBirds;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.Screen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main implements ApplicationListener {
-    Texture backgroundTexture;
-    Texture bucketTexture;
-    Texture dropTexture;
-    Sound dropSound;
-    Music music;
-
-    SpriteBatch spriteBatch;
-    FitViewport viewport;
-
-    Sprite bucketSprite; // Declare a new Sprite variable
-
-    Vector2 touchPos;
-
-    Array<Sprite> dropSprites;
-    float dropTimer;
-
-    Rectangle bucketRectangle;
-    Rectangle dropRectangle;
+public class Main extends Game {
+    private SpriteBatch batch;
+    private Texture image;
 
     @Override
     public void create() {
-        //create your application here
-        backgroundTexture = new Texture("background.png");
-        bucketTexture = new Texture("bucket.png");
-        dropTexture = new Texture("drop.png");
-
-        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
-        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
-
-        spriteBatch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
-
-        bucketSprite = new Sprite(bucketTexture); // Initialize the sprite based on the texture
-        bucketSprite.setSize(1, 1); // Define the size of the sprite
-
-        touchPos = new Vector2();
-        dropSprites = new Array<>();
-
-        bucketRectangle = new Rectangle();
-        dropRectangle = new Rectangle();
-
-        music.setLooping(true);
-        music.setVolume(.5f);
-        music.play();
+        batch = new SpriteBatch();
+//        image = new Texture("libgdx.png");
+        this.setScreen(new io.github.AngryBirds.HomeScreen(this));
     }
 
-    @Override
-    public void resize(int width, int height) {
-        viewport.update(width, height, true); // true centers the camera
-    }
+//    @Override
+//    public void render() {
+//        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+//        batch.begin();
+//        batch.draw(image, 140, 210);
+//        batch.end();
+//    }
+//
+//    @Override
+//    public void dispose() {
+//        batch.dispose();
+//        image.dispose();
+//    }
 
     @Override
     public void render() {
-        // Draw your application here.
-
-        // organize code into three methods
-        input();
-        logic();
-        draw();
-    }
-
-    private void input() {
-        float speed = 4f;
-        float delta = Gdx.graphics.getDeltaTime();
-
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            bucketSprite.translateX(speed * delta);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            bucketSprite.translateX(-speed * delta);
-        }
-
-        if (Gdx.input.isTouched()) {
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY()); // Get where the touch happened on screen
-            viewport.unproject(touchPos); // Convert the units to the world units of the viewport
-            bucketSprite.setCenterX(touchPos.x); // Change the horizontally centered position of the bucket
-        }
-    }
-
-
-
-    private void logic() {
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-        float bucketWidth = bucketSprite.getWidth();
-        float bucketHeight = bucketSprite.getHeight();
-
-        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
-
-        float delta = Gdx.graphics.getDeltaTime();
-        bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
-
-        for (int i = dropSprites.size - 1; i >= 0; i--) {
-            Sprite dropSprite = dropSprites.get(i);
-            float dropWidth = dropSprite.getWidth();
-            float dropHeight = dropSprite.getHeight();
-
-            dropSprite.translateY(-2f * delta);
-            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
-
-            if (dropSprite.getY() < -dropHeight) dropSprites.removeIndex(i);
-            else if (bucketRectangle.overlaps(dropRectangle)) {
-                dropSprites.removeIndex(i);
-                dropSound.play(); // Play the sound
-            }
-        }
-
-        dropTimer += delta;
-        if (dropTimer > 1f) {
-            dropTimer = 0;
-            createDroplet();
-        }
-    }
-
-    private void draw() {
-        ScreenUtils.clear(Color.BLACK);
-        viewport.apply();
-        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
-        spriteBatch.begin();
-
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-
-        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
-        bucketSprite.draw(spriteBatch);
-
-        // draw each sprite
-        for (Sprite dropSprite : dropSprites) {
-            dropSprite.draw(spriteBatch);
-        }
-
-        spriteBatch.end();
-    }
-
-    private void createDroplet() {
-        float dropWidth = 1;
-        float dropHeight = 1;
-        float worldWidth = viewport.getWorldWidth();
-        float worldHeight = viewport.getWorldHeight();
-
-        Sprite dropSprite = new Sprite(dropTexture);
-        dropSprite.setSize(dropWidth, dropHeight);
-        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth)); // Randomize the drop's x position
-        dropSprite.setY(worldHeight);
-        dropSprites.add(dropSprite);
-    }
-
-
-    @Override
-    public void pause() {
-        // Invoked when your application is paused.
-    }
-
-    @Override
-    public void resume() {
-        // Invoked when your application is resumed after pause.
+        super.render(); // Important to call this to render the active screen
     }
 
     @Override
     public void dispose() {
-        // Destroy application's resources here.
+        batch.dispose();
+        getScreen().dispose(); // Dispose of the active screen when done
     }
 }
