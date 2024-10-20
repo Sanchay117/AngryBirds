86ae0904e12a413438962c190c1e7d02a36fbdc8
AbhinavKashyap <162035178+Abhinav0821@users.noreply.github.com>
2024-10-20 14:40:51 +0530

    Update Main.java

--- core/src/main/java/io/github/AngryBirds/Main.java
+++ core/src/main/java/io/github/AngryBirds/Main.java
@@ -1,47 +1,21 @@
-
 package io.github.AngryBirds;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.Screen;
+import com.mygame.screens.HomeScreen;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends Game {
-    private SpriteBatch batch;
-    private Texture image;
-
+public class MainGame extends Game {
     @Override
     public void create() {
-        batch = new SpriteBatch();
-//        image = new Texture("libgdx.png");
-        this.setScreen(new io.github.AngryBirds.HomeScreen(this));
+        this.setScreen(new HomeScreen(this)); // Start with HomeScreen
     }
-
-//    @Override
-//    public void render() {
-//        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-//        batch.begin();
-//        batch.draw(image, 140, 210);
-//        batch.end();
-//    }
-//
-//    @Override
-//    public void dispose() {
-//        batch.dispose();
-//        image.dispose();
-//    }
 
     @Override
     public void render() {
-        super.render(); // Important to call this to render the active screen
+        super.render(); // This calls the render method of the active screen
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        getScreen().dispose(); // Dispose of the active screen when done
+        // Dispose any global resources here
     }
 }
