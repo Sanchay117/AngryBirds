dca3923d9bcc9f0dedd86595bad51b06c156d5a1
Sanchay Singh <sanchay072@gmail.com>
2024-10-20 16:40:10 +0530

    HOME SCREEN PROTO DONE

--- core/src/main/java/io/github/AngryBirds/Main.java
+++ core/src/main/java/io/github/AngryBirds/Main.java
@@ -1,22 +1,62 @@
-package com.mygdx.angrybirds;
+package io.github.AngryBirds;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
-import com.badlogic.gdx.Game;
-import com.mygame.screens.HomeScreen;
+public class Main extends ApplicationAdapter {
 
-public class MainGame extends Game {
+    enum Screen{
+        TITLE, MAIN_GAME, GAME_OVER;
+    }
+
+    Screen currentScreen = Screen.TITLE;
+
+    SpriteBatch batch;
+    ShapeRenderer shapeRenderer;
+    BitmapFont font;
+    Texture backgroundImage;
+    Music music;
+
     @Override
-    public void create() {
-        this.setScreen(new HomeScreen(this)); // Start with HomeScreen
+    public void create () {
+        batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
+        font = new BitmapFont();
+
+        // bg music
+        music = Gdx.audio.newMusic(Gdx.files.internal("bg_music.mp3"));
+        music.setLooping(true);
+        music.setVolume(.5f);
+        music.play();
     }
 
     @Override
-    public void render() {
-        super.render(); // This calls the render method of the active screen
+    public void render () {
+
+        if(currentScreen == Screen.TITLE){
+            backgroundImage = new Texture("bg.jpg");
+
+            batch.begin();
+
+            batch.draw(backgroundImage, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            batch.draw(new Texture("play.png"), (float) (Gdx.graphics.getWidth() /2 - 205 ),50,350,150);
+            batch.draw(new Texture("settings.jpg"), 0,50,150,50);
+
+            batch.end();
+        }
     }
 
     @Override
-    public void dispose() {
-        // Dispose any global resources here
+    public void dispose () {
+        shapeRenderer.dispose();
     }
 }
