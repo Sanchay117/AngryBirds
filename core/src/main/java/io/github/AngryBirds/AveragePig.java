9375f0ff5e67e1166f51cc5f25c5ddf7fa674be5
Sanchay Singh <sanchay072@gmail.com>
2024-10-25 15:23:12 +0530

    changes done

---
+++ core/src/main/java/io/github/AngryBirds/AveragePig.java
@@ -0,0 +1,9 @@
+package io.github.AngryBirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class AveragePig extends Pig{
+    public AveragePig(Texture texture, float x, float y, int width, int height) {
+        super(1,10,texture,x,y,width,height);
+    }
+}
--- core/src/main/java/io/github/AngryBirds/MediumPig.java
+++
@@ -1,4 +0,0 @@
-package io.github.AngryBirds;
-
-public class MediumPig {
-}
