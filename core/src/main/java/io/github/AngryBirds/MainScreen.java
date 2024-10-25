327d0668586b37d6ce8b5023d142df0268932bb5
AbhinavKashyap <162035178+Abhinav0821@users.noreply.github.com>
2024-10-25 12:47:34 +0530

    Update MainScreen.java added lock & co.

--- core/src/main/java/io/github/AngryBirds/MainScreen.java
+++ core/src/main/java/io/github/AngryBirds/MainScreen.java
@@ -16,10 +16,12 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.graphics.Color;
 
@@ -32,6 +34,7 @@ public class MainScreen extends ScreenAdapter {
     private Stage stage;
     private Texture backBtnTexture;
     private Texture lvlBtnTexture;
+    private Texture lockTexture;
     private ImageButton backBtn;
     private ImageButton lvlBtn1;
     private ImageButton lvlBtn2;
@@ -52,6 +55,7 @@ public class MainScreen extends ScreenAdapter {
 
         backBtnTexture = new Texture("back.png");
         lvlBtnTexture = new Texture("lvlBG.png");
+        lockTexture = new Texture("lock.png");  
 
         Skin skin = new Skin();
         skin.add("back", backBtnTexture);
@@ -67,28 +71,26 @@ public class MainScreen extends ScreenAdapter {
         lvlBtn3 = new ImageButton(lvlStyle);
         lvlBtn4 = new ImageButton(lvlStyle);
         lvlBtn5 = new ImageButton(lvlStyle);
-        
+
         backBtn.setPosition(0.05f, viewHeight - 200);
         backBtn.setSize(200, 200);
-        
+
         lvlBtn1.setSize(350, 350);
         lvlBtn2.setSize(350, 350);
         lvlBtn3.setSize(350, 350);
         lvlBtn4.setSize(350, 350);
         lvlBtn5.setSize(350, 350);
 
-       
-        float gap = (viewWidth - (3 * lvlBtn1.getWidth())) / 4;  
-        float rowHeight = viewHeight * 0.4f;  
-        float secondRowHeight = rowHeight - 250; 
+        float gap = (viewWidth - (3 * lvlBtn1.getWidth())) / 4;
+        float rowHeight = viewHeight * 0.4f;
+        float secondRowHeight = rowHeight - 250;
 
+        lvlBtn1.setPosition(gap, rowHeight);
+        lvlBtn2.setPosition(2 * gap + lvlBtn1.getWidth(), rowHeight);
+        lvlBtn3.setPosition(3 * gap + 2 * lvlBtn1.getWidth(), rowHeight);
 
-        lvlBtn1.setPosition(gap, rowHeight); 
-        lvlBtn2.setPosition(2 * gap + lvlBtn1.getWidth(), rowHeight); 
-        lvlBtn3.setPosition(3 * gap + 2 * lvlBtn1.getWidth(), rowHeight); 
-
-        lvlBtn4.setPosition(gap + lvlBtn1.getWidth() / 2, secondRowHeight);  
-        lvlBtn5.setPosition(2 * gap + lvlBtn1.getWidth() + lvlBtn4.getWidth() / 2, secondRowHeight);  
+        lvlBtn4.setPosition(gap + lvlBtn1.getWidth() / 2, secondRowHeight);
+        lvlBtn5.setPosition(2 * gap + lvlBtn1.getWidth() + lvlBtn4.getWidth() / 2, secondRowHeight);
 
         backBtn.addListener(new ClickListener() {
             @Override
@@ -110,28 +112,16 @@ public class MainScreen extends ScreenAdapter {
                 game.setScreen(new LevelScreen(game));
             }
         });
-
-        lvlBtn3.addListener(new ClickListener() {
+        ClickListener shakeListener = new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new LevelScreen(game));
+                shakeButton((ImageButton) event.getListenerActor());
             }
-        });
-
-        lvlBtn4.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new LevelScreen(game));
-            }
-        });
-
-        lvlBtn5.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new LevelScreen(game));
-            }
-        });
-
+        };
+        lvlBtn3.addListener(shakeListener);
+        lvlBtn4.addListener(shakeListener);
+        lvlBtn5.addListener(shakeListener);
+        // Add buttons to stage
         stage.addActor(backBtn);
         stage.addActor(lvlBtn1);
         stage.addActor(lvlBtn2);
@@ -151,8 +141,6 @@ public class MainScreen extends ScreenAdapter {
 
         game.font.draw(game.batch, "LEVELS", viewWidth * 0.38f, viewHeight * 0.95f);
 
-        GlyphLayout layout = new GlyphLayout();
-
         drawCenteredText(game.mediumFont, "1", lvlBtn1);
         drawCenteredText(game.mediumFont, "2", lvlBtn2);
         drawCenteredText(game.mediumFont, "3", lvlBtn3);
@@ -162,9 +150,20 @@ public class MainScreen extends ScreenAdapter {
         stage.act(delta);
         stage.draw();
 
+        drawLockOverlay(lvlBtn3);
+        drawLockOverlay(lvlBtn4);
+        drawLockOverlay(lvlBtn5);
+
         game.batch.end();
     }
-
+    private void shakeButton(ImageButton button) {
+        button.addAction(Actions.sequence(
+                Actions.moveBy(10, 0, 0.05f),  
+                Actions.moveBy(-20, 0, 0.05f), 
+                Actions.moveBy(20, 0, 0.05f),  
+                Actions.moveBy(-10, 0, 0.05f)  
+        ));
+    }
     private void drawCenteredText(BitmapFont font, String text, ImageButton button) {
         GlyphLayout layout = new GlyphLayout(font, text);
 
@@ -175,7 +174,14 @@ public class MainScreen extends ScreenAdapter {
 
         font.draw(game.batch, text, textX, textY);
     }
-    
+
+    private void drawLockOverlay(ImageButton button) {
+        float lockSize = 250;
+        float lockX = button.getX() + (button.getWidth() - lockSize) / 2;
+        float lockY = button.getY() + (button.getHeight() - lockSize) / 2-50;
+        game.batch.draw(lockTexture, lockX, lockY, lockSize, lockSize);
+    }
+
     @Override
     public void resize(int width, int height) {
         viewWidth = width;
@@ -186,6 +192,7 @@ public class MainScreen extends ScreenAdapter {
     public void hide(){
         Gdx.input.setInputProcessor(null);
         backBtnTexture.dispose();
+        lockTexture.dispose();
         stage.dispose();
     }
 }
