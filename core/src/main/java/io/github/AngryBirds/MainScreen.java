b25171ff1c77b78f5fc79751773c2c150eff6dda
AbhinavKashyap <162035178+Abhinav0821@users.noreply.github.com>
2024-10-25 09:42:29 +0530

    Update MainScreen.java formatting and resizing

--- core/src/main/java/io/github/AngryBirds/MainScreen.java
+++ core/src/main/java/io/github/AngryBirds/MainScreen.java
@@ -47,7 +47,7 @@ public class MainScreen extends ScreenAdapter {
     }
 
     @Override
-    public void show(){
+    public void show() {
         stage = new Stage();
 
         backBtnTexture = new Texture("back.png");
@@ -67,26 +67,28 @@ public class MainScreen extends ScreenAdapter {
         lvlBtn3 = new ImageButton(lvlStyle);
         lvlBtn4 = new ImageButton(lvlStyle);
         lvlBtn5 = new ImageButton(lvlStyle);
-
-        backBtn.setPosition(0.05f,viewHeight-200);
+        
+        backBtn.setPosition(0.05f, viewHeight - 200);
         backBtn.setSize(200, 200);
+        
+        lvlBtn1.setSize(350, 350);
+        lvlBtn2.setSize(350, 350);
+        lvlBtn3.setSize(350, 350);
+        lvlBtn4.setSize(350, 350);
+        lvlBtn5.setSize(350, 350);
 
-        lvlBtn1.setSize(250, 250);
-        lvlBtn2.setSize(250, 250);
-        lvlBtn3.setSize(250, 250);
-        lvlBtn4.setSize(250, 250);
-        lvlBtn5.setSize(250, 250);
+       
+        float gap = (viewWidth - (3 * lvlBtn1.getWidth())) / 4;  
+        float rowHeight = viewHeight * 0.4f;  
+        float secondRowHeight = rowHeight - 250; 
 
-        lvlBtn1.setPosition(viewWidth*x, viewHeight*0.5f);
-        x+=0.175f;
-        lvlBtn2.setPosition(viewWidth*x, viewHeight*0.5f);
-        x+=0.175f;
-        lvlBtn3.setPosition(viewWidth*x, viewHeight*0.5f);
-        x+=0.175f;
-        lvlBtn4.setPosition(viewWidth*x, viewHeight*0.5f);
-        x+=0.175f;
-        lvlBtn5.setPosition(viewWidth*x, viewHeight*0.5f);
-        x=0.08f;
+
+        lvlBtn1.setPosition(gap, rowHeight); 
+        lvlBtn2.setPosition(2 * gap + lvlBtn1.getWidth(), rowHeight); 
+        lvlBtn3.setPosition(3 * gap + 2 * lvlBtn1.getWidth(), rowHeight); 
+
+        lvlBtn4.setPosition(gap + lvlBtn1.getWidth() / 2, secondRowHeight);  
+        lvlBtn5.setPosition(2 * gap + lvlBtn1.getWidth() + lvlBtn4.getWidth() / 2, secondRowHeight);  
 
         backBtn.addListener(new ClickListener() {
             @Override
@@ -95,49 +97,39 @@ public class MainScreen extends ScreenAdapter {
             }
         });
 
-        lvlBtn1.addListener(new ClickListener(){
-
+        lvlBtn1.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new LevelScreen(game));
             }
-
         });
 
-        lvlBtn2.addListener(new ClickListener(){
-
+        lvlBtn2.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new LevelScreen(game));
             }
-
         });
 
-        lvlBtn3.addListener(new ClickListener(){
-
+        lvlBtn3.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new LevelScreen(game));
             }
-
         });
 
-        lvlBtn4.addListener(new ClickListener(){
-
+        lvlBtn4.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new LevelScreen(game));
             }
-
         });
 
-        lvlBtn5.addListener(new ClickListener(){
-
+        lvlBtn5.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new LevelScreen(game));
             }
-
         });
 
         stage.addActor(backBtn);
@@ -152,34 +144,38 @@ public class MainScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.begin();
+        game.batch.draw(game.background, 0, 0, viewWidth, viewHeight);
 
-        game.batch.draw(game.background, 0, 0,viewWidth,viewHeight);
-        game.font.draw(game.batch, "LEVELS", viewWidth*0.38f, viewHeight*0.95f);
+        game.font.draw(game.batch, "LEVELS", viewWidth * 0.38f, viewHeight * 0.95f);
+
+        GlyphLayout layout = new GlyphLayout();
+
+        drawCenteredText(game.mediumFont, "1", lvlBtn1);
+        drawCenteredText(game.mediumFont, "2", lvlBtn2);
+        drawCenteredText(game.mediumFont, "3", lvlBtn3);
+        drawCenteredText(game.mediumFont, "4", lvlBtn4);
+        drawCenteredText(game.mediumFont, "5", lvlBtn5);
 
         stage.act(delta);
         stage.draw();
 
-        game.mediumFont.draw(game.batch, "1", viewWidth*x - 120, viewHeight*.5f + 145);
-        x+=0.175f;
-        game.mediumFont.draw(game.batch, "2", viewWidth*x - 120, viewHeight*.5f + 145);
-        x+=0.175f;
-        game.mediumFont.draw(game.batch, "3", viewWidth*x - 120, viewHeight*.5f + 145);
-        x+=0.175f;
-        game.mediumFont.draw(game.batch, "4", viewWidth*x - 120, viewHeight*.5f + 145);
-        x+=0.175f;
-        game.mediumFont.draw(game.batch, "5", viewWidth*x - 120, viewHeight*.5f + 145);
-
-        x=0.23f;
-
         game.batch.end();
-
-
     }
 
+    private void drawCenteredText(BitmapFont font, String text, ImageButton button) {
+        GlyphLayout layout = new GlyphLayout(font, text);
+
+        float textX = button.getX() + (button.getWidth() - layout.width) / 2;
+        float textY = button.getY() + (button.getHeight() + layout.height) / 2;
+
+        textY -= layout.height / 2;
+
+        font.draw(game.batch, text, textX, textY);
+    }
+    
     @Override
     public void resize(int width, int height) {
         viewWidth = width;
