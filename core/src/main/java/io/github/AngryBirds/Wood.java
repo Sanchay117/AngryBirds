4094d8b9bc1cbc1c859b7318e06f9021f71a385b
Sanchay Singh <sanchay072@gmail.com>
2024-10-25 15:11:05 +0530

    changes done

---
+++ core/src/main/java/io/github/AngryBirds/Wood.java
@@ -0,0 +1,9 @@
+package io.github.AngryBirds;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Wood extends Material{
+    public Wood(Texture texture, float x, float y, int width, int height) {
+        super("Wood",texture,x,y,width,height);
+    }
+}
